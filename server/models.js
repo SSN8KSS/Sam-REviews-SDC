13a7c0eb64dbd50435ec661c8b7f5adc986494a8
gcking <george.king823@gmail.com>
2020-06-19 12:05:49 -0700

    Refactor APIs to retrieve data for singular hotel_id

--- server/models.js
+++ server/models.js
@@ -1,25 +1,23 @@
 const db = require('../database/index.js');
 const mysql = require('mysql');
 
-var getReviewData = (callback) => {
-  // may refactor to specify hotel_id for which reviews are retrieved
-  let sql = 'SELECT reviews.*, hotels.hotel_name, hotels.hotel_city, users.username, users.user_avatar, users.user_city, users.user_contributions, users.user_helpful_votes FROM reviews JOIN hotels ON reviews.hotel_id = hotels.id JOIN users ON reviews.user_id = users.id';
+var getReviewData = (callback, hotelId = 1) => {
+  let sql = 'SELECT reviews.*, hotels.hotel_name, hotels.hotel_city, users.username, users.user_avatar, users.user_city, users.user_contributions, users.user_helpful_votes FROM reviews JOIN hotels ON reviews.hotel_id = hotels.id JOIN users ON reviews.user_id = users.id WHERE hotels.id = ?';
 
-  db.query(sql, (err, results) => {
+  db.query(sql, [hotelId], (err, results) => {
     if (err) throw error;
     callback(results);
   });
 };
 
-var getQuestionData = (callback) => {
-  // may refactor to specify hotel_id for which Q&A data is retrieved
-  let questionSql = 'SELECT questions.*, hotels.hotel_name, hotels.hotel_city, users.username, users.user_avatar, users.user_city, users.user_contributions, users.user_helpful_votes FROM questions JOIN hotels ON questions.hotel_id = hotels.id JOIN users ON questions.user_id = users.id';
+var getQuestionData = (callback, hotelId = 1) => {
+  let questionSql = 'SELECT questions.*, hotels.hotel_name, hotels.hotel_city, users.username, users.user_avatar, users.user_city, users.user_contributions, users.user_helpful_votes FROM questions JOIN hotels ON questions.hotel_id = hotels.id JOIN users ON questions.user_id = users.id WHERE hotels.id = ?';
 
   let answersSql = 'SELECT answers.*, users.username, users.user_avatar, users.user_city FROM answers JOIN questions ON answers.question_id = questions.id JOIN users on answers.answerer_user_id = users.id';
 
   var questionAnswerPacket = {};
 
-  db.query(questionSql, (err, results) => {
+  db.query(questionSql, [hotelId], (err, results) => {
     if (err) throw err;
     questionAnswerPacket.questions = results;
 
