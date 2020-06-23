394f837790b1d1e90f7d47f2cceea74cca4dae7c
gcking <george.king823@gmail.com>
2020-06-22 18:44:57 -0700

    Write first Enzyme test

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,8 +1,10 @@
 import App from '../client/components/app.jsx';
+import renderer from 'react-test-renderer';
 
 describe('App', () => {
-  it('sums numbers', () => {
-    expect(1+2).toEqual(3);
-    expect(2+2).toEqual(4);
+  it('renders appropriately', () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
