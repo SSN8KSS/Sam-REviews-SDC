282f2329c97e4f94a64798471fa31fa9c5d54cc3
gcking <george.king823@gmail.com>
2020-06-26 09:07:53 -0700

    Implement further styling

--- client/components/review.jsx
+++ client/components/review.jsx
@@ -8,7 +8,7 @@ function Review(props) {
         <a href="#"><img src={props.reviewData.user_avatar} width="10" height="10"></img></a>
         <div className="review-header-text">
           <div className="review-header-text-name">
-            <div><a href="#"><strong>{props.reviewData.username}</strong></a>{` wrote a review ${moment(props.reviewData.review_date).format('LL')}`}</div>
+            <div><a href="#"><strong>{props.reviewData.username}</strong></a>{` wrote a review ${moment(props.reviewData.review_date).format('MMM YYYY')}`}</div>
           </div>
           <div className="review-header-text-container">
             <div className="user-map-icon-container"><img src="https://www.clker.com/cliparts/A/c/j/V/a/Q/google-map-pointer-grey-hi.png" alt="map-icon"/></div>
@@ -19,7 +19,7 @@ function Review(props) {
       <div className="review-body">
         <div>{props.reviewData.overall_rating}</div>
         <div className="review-body-text">{'"' + props.reviewData.review_body + '"'}</div>
-        <div className="review-date-of-stay"><strong>Date of stay: </strong>{moment(props.reviewData.date_of_stay).format('LL')}</div>
+        <div className="review-date-of-stay"><strong>Date of stay: </strong>{moment(props.reviewData.date_of_stay).format('MMMM YYYY')}</div>
       </div>
     </li>
   );
