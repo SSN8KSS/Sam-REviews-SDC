04a2a8ea97ace4a2296fb132aaf9342422a628ab
gcking <george.king823@gmail.com>
2020-06-29 16:02:47 -0700

    Change port number to 3004

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 3004;
 const models = require('./models.js');
 
 app.use('/', express.static('public'));
