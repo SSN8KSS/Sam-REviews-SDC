46f5a3955d8b49fdb984be3ce697d31de450aed2
gcking <george.king823@gmail.com>
2020-06-29 19:55:39 -0700

    Add build-db script to package.json

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const app = express();
 const port = 3004;
 const models = require('./models.js');
 
-app.use('/', express.static('public'));
+app.use('/reviews', express.static('public'));
 
 app.get('/reviews/:hotelId', (req, res) => {
+  // to do: filter hotelId to validate input
   const hotelId = req.params.hotelId || 1;
   models.getReviewData((results) => {
     res.status(200).send(JSON.stringify(results));
